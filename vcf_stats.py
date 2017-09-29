17b1b8447b15df01a8cf5b91765c8a1e206a7add
iainrb <ib5@sanger.ac.uk>
2017-09-29 08:46:53 +0100

    Read header, column heads, and body of VCF file

--- vcf_stats.py
+++ vcf_stats.py
@@ -6,51 +6,69 @@
 
 import re, sys, json
 
+
 def main():
 
-    stats = parse_stats(sys.stdin) # data structure
-    # stats has results for all individuals; write as JSON for each individual
+    parser = vcf_parser()
+    stats = parser.parse_stats(sys.stdin)
 
 
-def parse_stats(infile):
+class vcf_parser:
 
-    # want to read header (as a whole) and body (in chunks)
-    # need stats for each individual
-    stats = {}
 
-    buffer_size = 50 * 10**6 # input buffer size, in bytes
+    def __init__(self):
+        self.buffer_size = 50 * 10**6 # input buffer size, in bytes
 
-    input_lines = infile.readlines(buffer_size)
+
+    def parse_stats(self, infile):
+
+        # want to read header (as a whole) and body (in chunks)
+        # need stats for each individual
+        stats = {}
+
+        # read VCF header
+        header_lines = []
+        column_heads = None
+
+        while True:
+            # read header lines one at a time
+            line = infile.readline()
+            if line == '':
+                msg = "Reached end of file without finding end of VCF header"
+                raise ValueError(msg)
+            if re.match('##', line):
+                header_lines.append(line)
+            elif re.match('#CHROM', line):
+                column_heads = line
+                break
+            else:
+                msg = "Unexpected line in VCF header; line "+\
+                    "does not start with '##' or '#CHROM': "+line
+                raise ValueError(msg)
+
+        sys.stderr.write("Read "+str(len(header_lines))+\
+                             " lines in VCF header\n")
+        if column_heads != None:
+            sys.stderr.write("Read VCF column headers\n")
+
+        # next line will be start of VCF body
+
+        # TODO warn when input exceeds eg. 1 TB?
     
-    # read VCF header
-
-    input_lines = infile.readlines(buffer_size)
-    header_lines = []
-    column_heads = None
-    for line in input_lines:
-        if re.match('##', line):
-            header_lines.append(line)
-        elif re.match('#CHROM', line):
-            column_heads = line
-            break
-        else:
-            msg = "Unexpected line in VCF header; line does not start "+\
-                  "with '##' or '#CHROM': "+line
-            raise ValueError(msg)
-    # next line will be first line in VCF body
-
-    # TODO warn when input exceeds eg. 1 TB?
-    
-    # read VCF body
-    while True:
-        lines = infile.readlines(buffer_size)
-        if lines == []: break
-        for line in lines:
-            fields = parse_body_line(line)
-            stats = update_stats(fields)
-
-    return stats
+        # read VCF body
+        line_count = 0
+        while True:
+            lines = infile.readlines(self.buffer_size)
+            if lines == []: break
+            for line in lines:
+                line_count += 1
+                #fields = parse_body_line(line)
+                #stats = update_stats(fields)
+        sys.stderr.write("Read "+str(line_count)+" lines in VCF body\n")
+        return stats
             
+
+# end of vcf_parser
 
 if __name__ == "__main__":
     main()
